a9bb40ccd2e1cdde95752c2d534982cb47ae4787
saya <772683180ao@gmail.com>
2024-04-19 12:40:45 +0800

    Theme.kt

--- app/src/main/java/site/sayaz/ts3client/ui/theme/Theme.kt
+++ app/src/main/java/site/sayaz/ts3client/ui/theme/Theme.kt
@@ -1,11 +1,20 @@
 package site.sayaz.ts3client.ui.theme
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 import site.sayaz.ts3client.ui.theme.Typography
 
 
@@ -79,11 +88,27 @@ fun TS3ClientTheme(
   useDarkTheme: Boolean = isSystemInDarkTheme(),
   content: @Composable() () -> Unit
 ) {
-  val colors = if (!useDarkTheme) {
-    LightColors
-  } else {
-    DarkColors
-  }
+    // 动态配色
+    val context = LocalContext.current
+    val view = LocalView.current
+    val colors = when {
+        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
+            if (useDarkTheme) dynamicDarkColorScheme(context)
+            else dynamicLightColorScheme(context)
+        }
+        useDarkTheme -> DarkColors
+        else -> LightColors
+    }
+    // 状态栏
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colors.primary.toArgb()
+            WindowCompat
+                .getInsetsController(window, view)
+                .isAppearanceLightStatusBars = useDarkTheme
+        }
+    }
 
   MaterialTheme(
     colorScheme = colors,
